9e63c3c04e481ce53f925abd8d83f88181e92982
jijibb99 <67882900+jijibb99@users.noreply.github.com>
2021-02-24 15:54:47 +0900

    Add files via upload

---
+++ mypage/src/main/java/baedalteamfive/MypageRepository.java
@@ -0,0 +1,8 @@
+package baedalteamfive;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MypageRepository extends CrudRepository<Mypage, Long> {
+    Mypage findByOrderId(Long orderId);
+    Mypage findByDeliveryId(Long deliveryId);
+}
